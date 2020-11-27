90873640ba63bf7e980ad3a6bbc1433d74141fb0
Will-Helliwell <68912961+Will-Helliwell@users.noreply.github.com>
2020-11-27 09:20:04 +0000

    refactor - abstract empty? and full? private methods from release_bike and dock_bike respectively

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -8,13 +8,23 @@ class DockingStation
   end
 
   def release_bike
-    fail "No bikes available" if @bikes == []
+    fail "No bikes available" if self.empty?
     @bikes.shift
   end
 
   def dock_bike(bike)
     # Use fail or raise to raise an error
-    fail "Station Full" if @bikes.length > 19
+    fail "Station Full" if self.full?
     @bikes << bike
   end
+
+  private
+  def full?
+    @bikes.length >= 20 ? true : false
+  end
+
+  def empty?
+    @bikes.length == 0 ? true : false
+  end
+
 end
